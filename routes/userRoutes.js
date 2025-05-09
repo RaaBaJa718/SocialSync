a1d9a27b53222c25492723fd43d90101513737de
Ruben <ruben.alston646@gmail.com>
2025-05-08 21:30:49 -0400

    Refactor user routes to remove test route and integrate user controller functions

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
+
 const router = express.Router();
 
-// Test Route - Ensure it's working
-router.get("/", (req, res) => {
-  res.send("Users endpoint working!");
-});
+// Define routes
+router.get("/", getAllUsers); // Get all users
+router.get("/:userId", getUserById); // Get a user by ID
+router.post("/", createUser); // Create a new user
+router.put("/:userId", updateUser); // Update a user by ID
+router.delete("/:userId", deleteUser); // Delete a user by ID
 
 module.exports = router;
