a28776d99f7ba0a95f1df2a1702dc0afa1c6f47e
Ruben <ruben.alston646@gmail.com>
2025-05-08 21:23:41 -0400

    Define User model with schema and virtual for friend count

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+// Define User Schema
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Please enter a valid email"],
+  },
+  thoughts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+},
+{
+  toJSON: { virtuals: true },
+});
+
+// Virtual to get the count of friends
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// Export the model
+module.exports = mongoose.model("User", UserSchema);
