4548ae1714a541bbf179e0c274eeae4087d2405f
Ruben <ruben.alston646@gmail.com>
2025-05-09 18:06:33 -0400

    Add addFriend function to userController for adding friends

--- controllers/userController.js
+++ controllers/userController.js
@@ -53,6 +53,24 @@ async function deleteUser(req, res) {
   }
 }
 
+async function addFriend(req, res) {
+  try {
+    const user = await User.findById(req.params.userId);
+    const friend = await User.findById(req.params.friendId);
+
+    if (!user || !friend) {
+      return res.status(404).json({ message: "User or Friend not found" });
+    }
+
+    user.friends.push(friend._id);
+    await user.save();
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Error adding friend", error });
+  }
+}
+
 // Export all functions for use in routes
 module.exports = {
   getAllUsers,
